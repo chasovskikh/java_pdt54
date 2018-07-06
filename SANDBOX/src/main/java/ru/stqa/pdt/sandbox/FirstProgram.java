3ae3a086f8c377845d0cede62bb57b84130c5e7b
Chsovskikh Ann <nuka97@mail.ru>
2018-07-06 18:41:39 +0600

    Добавлено несколько функций

--- SANDBOX/src/main/java/ru/stqa/pdt/sandbox/FirstProgram.java
+++ SANDBOX/src/main/java/ru/stqa/pdt/sandbox/FirstProgram.java
@@ -3,7 +3,28 @@ package ru.stqa.pdt.sandbox;
 public class FirstProgram {
 
 
-	public static void main(String[] args){
-		System.out.println("Hello, world!");
-	}
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Anna");
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double x = 3;
+    double y = 4;
+    System.out.println("Площадь прямоугольника со сторонами " + x + " и " + y + " = " + area(x, y));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello," + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double x, double y) {
+    return x * y;
+  }
 }
