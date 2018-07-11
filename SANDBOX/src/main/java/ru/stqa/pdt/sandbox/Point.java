ab951186509e28f6a766a081c4f5995aa4bcac2a
Chsovskikh Ann <nuka97@mail.ru>
2018-07-11 21:51:31 +0600

    upd

--- SANDBOX/src/main/java/ru/stqa/pdt/sandbox/Point.java
+++ SANDBOX/src/main/java/ru/stqa/pdt/sandbox/Point.java
@@ -11,7 +11,7 @@ public class Point {
     this.y = y;
   }
 
-  double distance(Point p) {
+  public double distance(Point p) {
 
     return Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2));
   }
