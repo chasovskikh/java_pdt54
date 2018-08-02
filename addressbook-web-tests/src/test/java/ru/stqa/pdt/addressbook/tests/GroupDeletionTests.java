cb77ff8cad69fc74e015139f93f78c5381ccfe46
Chsovskikh Ann <nuka97@mail.ru>
2018-08-02 20:54:53 +0600

    Реструктуризация кода

--- addressbook-web-tests/src/test/java/ru/stqa/pdt/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pdt/addressbook/tests/GroupDeletionTests.java
@@ -1,20 +1,24 @@
 package ru.stqa.pdt.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.stqa.pdt.addressbook.model.ContactData;
 import ru.stqa.pdt.addressbook.model.GroupData;
 
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
 
-
+  @BeforeMethod
+  public void ensurePreconditions() {
+    app.getNavigationHelper().gotoHomePage();
+    if (!app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactData("Ann", "Chasovskikh", "ann_chasovskikh", "Novosibirsk", "+78548960052", "annChas@gmail.com", "test1"));
+    }
+  }
   @Test
   public void testGroupDeletion() {
-    app.getNavigationHelper().gotoGroupPage();
-    if (!app.getGroupHelper().isThereAGroup()) {
-      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-    }
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
