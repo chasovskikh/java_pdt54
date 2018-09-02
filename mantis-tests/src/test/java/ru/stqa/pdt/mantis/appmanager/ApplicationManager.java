8753f7b71aedefbf92597d755fa758845f419878
Chsovskikh Ann <nuka97@mail.ru>
2018-09-02 23:51:37 +0600

    не приходит письмо

--- mantis-tests/src/test/java/ru/stqa/pdt/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pdt/mantis/appmanager/ApplicationManager.java
@@ -23,6 +23,7 @@ public class ApplicationManager {
   private String browser;
   private RegistrationHelper registrationHelper;
   private FtpHelper ftp;
+  private JamesHelper jamesHelper;
 
 
   public ApplicationManager(String browser) {
@@ -88,5 +89,12 @@ public class ApplicationManager {
       mailHelper = new MailHelper(this);
     }
     return mailHelper;
+  }
+
+  public JamesHelper james() {
+    if (jamesHelper == null) {
+      jamesHelper = new JamesHelper(this);
+    }
+    return jamesHelper;
   }
 }
