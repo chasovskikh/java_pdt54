a2605a0021b163c53b270b2081aed05e915bd429
Chsovskikh Ann <nuka97@mail.ru>
2018-09-09 18:39:03 +0600

    retest for jenkins

--- SANDBOX/src/test/java/ru/stqa/pdt/sandbox/PointTests1.java
+++ SANDBOX/src/test/java/ru/stqa/pdt/sandbox/PointTests1.java
@@ -11,6 +11,6 @@ public class PointTests1 {
     //Error, must be 10.568348972285122
     Point p3 = new Point(0, -0.7);
     Point p4 = new Point(6, 8);
-    Assert.assertEquals(p3.distance(p4), 10);
+    Assert.assertEquals(p3.distance(p4), 10.568348972285122);
   }
 }
